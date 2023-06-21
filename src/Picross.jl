d8c2b3ea720b0f802634a21beff97ebf94a092df
lassepe <lasse.peters@mailbox.org>
2023-06-21 12:53:15 +0200

    Fix gui rotation

--- src/Picross.jl
+++ src/Picross.jl
@@ -15,15 +15,30 @@ end
 function show_gui(problem::Problem, problem_state::ProblemState)
     @assert length(problem.row_blocks) == size(problem_state.grid, 1)
     @assert length(problem.column_blocks) == size(problem_state.grid, 2)
-    Makie.heatmap(problem_state.grid; axis=(; aspect=1))
+    figure, axis, heatmap = Makie.heatmap(
+        problem_state.grid'[:, end:-1:begin];
+        axis = (;
+            aspect = Makie.DataAspect(),
+            xticks = 0.5:1:(length(problem.row_blocks) + 0.5),
+            yticks = 0.5:1:(length(problem.column_blocks) + 0.5),
+            xgridwidth = 5,
+            ygridwidth = 5,
+            xgridcolor = :black,
+            ygridcolor = :black,
+        ),
+    )
+    Makie.translate!(heatmap, 0, 0, -100)
+
+    figure
 end
 
 function main()
     problem = Problem([3, 3, 3], [3, 3, 3])
-    problem_state = ProblemState(
-        [0 1 0
-            1 0 0
-            0 0 1])
+    problem_state = ProblemState([
+        1 0 1
+        0 0 1
+        0 0 1
+    ])
     show_gui(problem, problem_state)
 end
 
