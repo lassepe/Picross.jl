6e8d073f80d49a99b4a961a88c24330225cf9352
lassepe <lasse.peters@mailbox.org>
2023-06-21 15:57:09 +0200

    Generate all possible combinations for a given line

--- src/Picross.jl
+++ src/Picross.jl
@@ -2,6 +2,7 @@ module Picross
 
 using GLMakie: GLMakie
 using Makie: Makie
+using Combinatorics: Combinatorics
 
 struct Problem
     row_blocks::Vector{Vector{Int}}
@@ -39,6 +40,50 @@ function get_blocks_from_grid(grid::AbstractMatrix{Bool})
     (; row_blocks, column_blocks)
 end
 
+# TODO
+function is_valid(problem, problem_state)
+    current_blocks = get_blocks_from_grid(problem_state.grid)
+end
+
+"""
+Enumerate all possible ways to fill in the current line given the blocks.
+"""
+function get_possible_lines_from_blocks(blocks::Vector{Int}, line_length::Int)
+    min_free_space = length(blocks) - 1
+    free_space = line_length - sum(blocks)
+    @assert(
+        free_space >= min_free_space,
+        """
+        The blocks $(blocks) are too long for a line of length $(line_length).
+        Need at least $(min_free_space),
+        but only $(free_space) is left.
+        """
+    )
+    # go over all possibilities of distributing the excess free space between the blocks
+    # Note: +2 for virtual gaps in each end
+    free_space_partitions = collect(Combinatorics.partitions(free_space + 2, length(blocks) + 1))
+    gap_size_possibilities =
+        unique(reduce(vcat, [Combinatorics.permutations(p) for p in free_space_partitions]))
+
+    # at the extremes we can also have gaps of size zero (which we have accounted for through the offset of +2 above)
+    for p in gap_size_possibilities
+        p[begin] -= 1
+        p[end] -= 1
+    end
+
+    # TODO: continue here -- generate all boolean lines
+    map(gap_size_possibilities) do gap_size_possibility
+        line = zeros(Bool, line_length)
+        current_position = 1
+        for (block, gap) in zip(blocks, gap_size_possibility)
+            current_position += gap
+            line[current_position:(current_position + block - 1)] .= true
+            current_position += block
+        end
+        line
+    end
+end
+
 function show_gui(problem::Problem, problem_state::ProblemState)
     @assert length(problem.row_blocks) == size(problem_state.grid, 1)
     @assert length(problem.column_blocks) == size(problem_state.grid, 2)
@@ -62,7 +107,7 @@ end
 function main()
     problem = Problem([[3], [3], [3]], [[3], [3], [3]])
     problem_state = ProblemState([
-        1 1 1
+        1 0 1
         0 1 1
         1 0 1
     ])
